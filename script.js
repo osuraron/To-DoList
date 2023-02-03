6c5977029649b97911fc354eac1e1906fb16b0f0
osuraronath <osura.ronath@gmail.com>
2023-02-03 23:03:16 +0530

    list app updated

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 const deleteListButton = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitleElement = document.querySelector('[data-list-title]')
+const tasksContainer = document.querySelector('[data-tasks]')
+const taskTemplate = document.getElementById('task-template')
+const newTaskForm = document.querySelector('[data-new-task-form]')
+const newTaskInput = document.querySelector('[data-new-task-input]')
+const listCountElement = document.querySelector('[data-list-count]')
+const clearComplete = document.querySelector('[data-clear-complete-tasks-button]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
@@ -15,6 +23,16 @@ listsContainer.addEventListener('click', e=> {
     }
 })
 
+tasksContainer.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'input'){
+        const selectedList = lists.find(list => list.id === selectedListId)
+        const selectedTask = selectedList.tasks.find(task => task.id === e.target.id)
+        selectedTask.complete = e.target.checked
+        save()
+        renderTaskCount(selectedList)
+    }
+})
+
 newListForm.addEventListener("submit", e=>{
     e.preventDefault()
     const listName = newListInput.value
@@ -24,6 +42,28 @@ newListForm.addEventListener("submit", e=>{
     lists.push(list)
     saveAndRender()
 })
+
+newTaskForm.addEventListener("submit", e=>{
+    e.preventDefault()
+    const taskName = newTaskInput.value
+    if(taskName == null || taskName === '') return 
+    const task = createTask(taskName)
+    newTaskInput.value = null
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks.push(task)
+    saveAndRender()
+})
+
+function createTask(name){
+    return {id: Date.now().toString(), name: name, complete: false }
+}
+
+clearComplete.addEventListener('click', e=> {
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks = selectedList.tasks.filter(task => !task.complete)
+    saveAndRender()
+})
+
 
 deleteListButton.addEventListener("click", e=>{
  lists = lists.filter(list => list.id !== selectedListId)
@@ -44,9 +84,42 @@ function save(){
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
     localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
-
 function render(){
     clearElement(listsContainer)
+    renderLists()
+
+   const selectedList = lists.find(list => list.id === selectedListId)
+    if(selectedListId == null || !listDisplayContainer){
+        return
+    } else {
+        listDisplayContainer.style.display = ''
+        listTitleElement.innerText = selectedList.name
+        renderTaskCount(selectedList)
+        clearElement(tasksContainer)
+        renderTasks(selectedList)
+    }
+}
+
+function renderTasks(selectedList){
+    selectedList.tasks.forEach(task => {
+        const taskElement = document.importNode(taskTemplate.content,true)
+        const checkbox = taskElement.querySelector('input')
+        checkbox.id = task.id
+        checkbox.checked = task.complete
+        const label = taskElement.querySelector('label')
+        label.htmlFor = task.id
+        label.append(task.name)
+        tasksContainer.appendChild(taskElement)
+    })
+}
+
+function renderTaskCount(selectedList){
+    const incompleteTasks = selectedList.tasks.filter(task => !task.complete).length
+    const taskString = incompleteTasks === 1 ? "task" : "tasks"
+    listCountElement.innerText = `${incompleteTasks} ${taskString} remaining`
+}
+
+function renderLists(){
     lists.forEach(list => {
         const listElement = document.createElement('li')
         listElement.dataset.listId = list.id
